e18b973b07fabe724fe3ea1f829a40e6d11f5b4b
Rubab Malik <malikrubab@gmail.com>
2018-11-09 16:41:48 -0500

    - backbone structure of pipeline

--- BrightfieldPipeline.py
+++ BrightfieldPipeline.py
@@ -19,6 +19,10 @@ from scipy.ndimage.filters import gaussian_filter
 import sklearn.preprocessing as skp
 from skimage.morphology import watershed
 from skimage.feature import peak_local_max
+from skimage.filters import threshold_otsu, rank
+from skimage.util import img_as_ubyte
+from skimage.morphology import disk
+
 
 import sys
 # CREDIT: Lab's Git
@@ -131,7 +135,8 @@ def binarize3D(image, layers, dimensions):
         plotImage(stack)
 
         # binLayer = master.lib.processing.improved_watershed(stack, intensity)
-        binLayer = watershedCell(stack)
+        # binLayer = watershedCell(stack)
+        binLayer = otsu_binarize(stack)
         plotImage(binLayer)
         if counter == 1:
             base = binLayer
@@ -139,7 +144,36 @@ def binarize3D(image, layers, dimensions):
             base = np.concatenate((base, binLayer), axis=0)
     return base
 
-def otsu_binar
+def otsu_binarize(img):
+    radius = 15
+    selem = disk(radius)
+    local_otsu = rank.otsu(img, selem)
+    threshold_global_otsu = threshold_otsu(img)
+    global_otsu = img >= threshold_global_otsu
+
+
+    fig, ax = plt.subplots(2, 2, figsize=(8, 5))
+    ax1, ax2, ax3, ax4 = ax.ravel()
+
+    fig.colorbar(ax1.imshow(img, cmap=plt.cm.gray),
+               ax=ax1, orientation='horizontal')
+    ax1.set_title('Original')
+    ax1.axis('off')
+
+    fig.colorbar(ax2.imshow(local_otsu, cmap=plt.cm.gray),
+               ax=ax2, orientation='horizontal')
+    ax2.set_title('Local Otsu (radius=%d)' % radius)
+    ax2.axis('off')
+
+    ax3.imshow(img >= local_otsu, cmap=plt.cm.gray)
+    ax3.set_title('Original >= Local Otsu' % threshold_global_otsu)
+    ax3.axis('off')
+
+    ax4.imshow(global_otsu, cmap=plt.cm.gray)
+    ax4.set_title('Global Otsu (threshold = %d)' % threshold_global_otsu)
+    ax4.axis('off')
+
+    plt.show()
 def mainProcessing(pathname):
 
     # Find and import images as numpy arrays
